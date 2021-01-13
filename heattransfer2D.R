33b31b23f92bec2fffdec5e643f69e8c0bee42e4
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-01-13 21:23:58 +0100

    Add files via upload

--- heattransfer2D.R
+++ heattransfer2D.R
@@ -4,7 +4,7 @@
 library(png)
 library(stringr)
 
-object="letters"
+object="circiter"
 setwd(paste0("D:/R/43_HeatTransfer/",object,"/"))
 
 
@@ -53,16 +53,24 @@ plot(as.raster(heatobjectsunique/NOBJECTS), interpolate=F)
 # Plot scene in lines
 heatobjectslines=heatobjectsunique*0
 heatobjectslines[2:(NROW-1),2:(NCOL-1)]=
-    abs(heatobjectsunique[3:NROW,2:(NCOL-1)] - heatobjectsunique[2:(NROW-1),2:(NCOL-1)])+
-    abs(heatobjectsunique[2:(NROW-1),3:NCOL] - heatobjectsunique[2:(NROW-1),2:(NCOL-1)])+
-    abs(heatobjectsunique[1:(NROW-2),2:(NCOL-1)] - heatobjectsunique[2:(NROW-1),2:(NCOL-1)])*0+
-    abs(heatobjectsunique[2:(NROW-1),1:(NCOL-2)] - heatobjectsunique[2:(NROW-1),2:(NCOL-1)])*0
+    abs(
+        heatobjectsunique[3: NROW,   2:(NCOL-1)] -
+        heatobjectsunique[2:(NROW-1),2:(NCOL-1)]) +
+    abs(
+        heatobjectsunique[2:(NROW-1),3: NCOL] -
+        heatobjectsunique[2:(NROW-1),2:(NCOL-1)]) +
+    abs(
+        heatobjectsunique[1:(NROW-2),2:(NCOL-1)] -
+        heatobjectsunique[2:(NROW-1),2:(NCOL-1)])*0 +
+    abs(
+        heatobjectsunique[2:(NROW-1),1:(NCOL-2)] -
+        heatobjectsunique[2:(NROW-1),2:(NCOL-1)])*0
 heatobjectslines[heatobjectslines!=0]=1
-
 writePNG(heatobjectslines, paste0(object,"_lines.png"))
 
-# Define working arrays
-# alpha = k/(rho*cp), rho*cp=k/alpha
+
+# Precalculate working arrays
+# alpha=k/(rho*cp), rho*cp=k/alpha
 T=heatobjectsunique*0
 k=T
 alpha=T
@@ -71,10 +79,10 @@ for (i in 1:NOBJECTS) {
     k[lst[[i]]]=heatobjobjparams$k[i]
     alpha[lst[[i]]]=heatobjobjparams$alpha[i]
 }
-rhocp=k/alpha  # won't use alpha, just k and rhocp=rho * cp
+rhocp=k/alpha  # won't use alpha, just k and rhocp=rho*cp
 
 
-# Time domain T iteration (vector notation)
+# Time domain T iteration (matrix notation)
 MINT=min(heatobjobjparams$T)
 MAXT=max(heatobjobjparams$T)
 
@@ -84,18 +92,22 @@ for (j in 0:N) {
     if (j %% SKIP==0) {
         nombre=paste0("heattransfer_",
                       str_pad(j, nchar(N), pad='0'), ".png")
-        writePNG((T-MINT)/(MAXT-MINT), target=nombre)
+        writePNG((T-MINT)/(MAXT-MINT), nombre)
     }
 
-    # Formulation valid for multilayer walls
+    # Iterate T in the grid (formulation valid for multilayer walls)
     T[2:(NROW-1),2:(NCOL-1)] = T[2:(NROW-1),2:(NCOL-1)] +
         dt/(rhocp[2:(NROW-1),2:(NCOL-1)] * dx^2) *
         (
-            (k[3:NROW    ,2:(NCOL-1)] + k[2:(NROW-1),2:(NCOL-1)])/2 * (T[3:NROW    ,2:(NCOL-1)] - T[2:(NROW-1),2:(NCOL-1)]) -
-            (k[1:(NROW-2),2:(NCOL-1)] + k[2:(NROW-1),2:(NCOL-1)])/2 * (T[2:(NROW-1),2:(NCOL-1)] - T[1:(NROW-2),2:(NCOL-1)]) +
+            (k[3: NROW   ,2:(NCOL-1)] + k[2:(NROW-1),2:(NCOL-1)])/2 *
+            (T[3: NROW   ,2:(NCOL-1)] - T[2:(NROW-1),2:(NCOL-1)]) +
+            (k[1:(NROW-2),2:(NCOL-1)] + k[2:(NROW-1),2:(NCOL-1)])/2 *
+            (T[1:(NROW-2),2:(NCOL-1)] - T[2:(NROW-1),2:(NCOL-1)]) +
                 
-            (k[2:(NROW-1),3:NCOL    ] + k[2:(NROW-1),2:(NCOL-1)])/2 * (T[2:(NROW-1),3:NCOL    ] - T[2:(NROW-1),2:(NCOL-1)]) -
-            (k[2:(NROW-1),1:(NCOL-2)] + k[2:(NROW-1),2:(NCOL-1)])/2 * (T[2:(NROW-1),2:(NCOL-1)] - T[2:(NROW-1),1:(NCOL-2)])             
+            (k[2:(NROW-1),3: NCOL   ] + k[2:(NROW-1),2:(NCOL-1)])/2 *
+            (T[2:(NROW-1),3: NCOL   ] - T[2:(NROW-1),2:(NCOL-1)]) +
+            (k[2:(NROW-1),1:(NCOL-2)] + k[2:(NROW-1),2:(NCOL-1)])/2 *
+            (T[2:(NROW-1),1:(NCOL-2)] - T[2:(NROW-1),2:(NCOL-1)])             
         )
     
     # Reset T in the boundaries and apply instantaneous convection on fluids
@@ -103,58 +115,7 @@ for (j in 0:N) {
         if (heatobjobjparams$type[i]=='boundary') {
             T[lst[[i]]]=heatobjobjparams$T[i]  # boundary T conditions
         } else if (heatobjobjparams$type[i]=='fluid') {
-            T[lst[[i]]]=mean(T[lst[[i]]])  # average T = quick convection           
+            T[lst[[i]]]=mean(T[lst[[i]]])  # average T = energy conservation           
         }
     }
 }
-
-
-
-# Algoritmo:
-# NOTA: es obligatorio que todo el perímetro sea 'boundary'
-#       como aprox. el perímetro también puede ser 'fluid'
-# 1. Iterar T en todos los puntos salvo periferia (vectorización)
-# 2. Sobreescribir T en los objetos 'boundary' (which() precalculados)
-# 3. Promediar T en los objetos 'fluid' (which() precalculados)
-#
-# La periferia debería haberse reescrito en 2 o 3 (preferiblemente 2)
-
-# Mejoras a introducir:
-# 1. Fuentes de calor internas
-# 2. Convección en fluidos
-# 3. Cálculos de flujo de calor (vectorial)
-
-
-
-
-
-
-
-
-NROW=60
-NCOL=80
-a=array(1:48,c(NROW,NCOL))
-
-# Method 1: which() indexing
-indices=which(col(a)>1 & col(a)<ncol(a) & row(a)>1 & row(a)<NROW)
-# a[indices+1] baja una fila
-# a[indices-1] sube una fila
-# a[indices+NROW] avanza una columna
-# a[indices-NROW] retrocede una columna
-start_time=Sys.time()
-for (i in 1:100000) a[indices]=a[indices]+
-    (a[indices+1] + a[indices-1] +
-     a[indices+NROW] + a[indices-NROW])/1000
-end_time=Sys.time()
-print(end_time - start_time)
-
-# Method 2: array indexing
-start_time=Sys.time()
-for (i in 1:10000) a[2:(NROW-1),2:(NCOL-1)]=a[2:(NROW-1),2:(NCOL-1)]+
-    (a[3:NROW,2:(NCOL-1)] + a[1:(NROW-2),2:(NCOL-1)] +
-     a[2:(NROW-1),3:NCOL] + a[2:(NROW-1),1:(NCOL-2)])/1000
-end_time=Sys.time()
-print(end_time - start_time)
-
-# indices: 21.06793 secs
-# matricial: 5.608142 secs -> ELEGIMOS ESTA FORMA
